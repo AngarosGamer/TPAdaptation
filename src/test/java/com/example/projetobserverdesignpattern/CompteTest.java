fb46761293262281618bbefb4102072bb3220140
Gabriel Houdry--Boheme <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2023-12-02 12:06:33 -0500

    Modified tests to use dependency injections

--- src/test/java/com/example/projetobserverdesignpattern/CompteTest.java
+++ src/test/java/com/example/projetobserverdesignpattern/CompteTest.java
@@ -2,8 +2,10 @@ package com.example.projetobserverdesignpattern;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
@@ -12,23 +14,12 @@ import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 public class CompteTest {
 
-    /* Configuration des beans */
     @Configuration
     static class CompteConfig {
 
-        @Bean
-        public Client client() { return new Client(); }
-        @Bean
-        public ConseillerFinancier conseiller() { return new ConseillerFinancier(); }
-        @Bean
-        public Compte compte(ConseillerFinancier conseiller, Client client) {
-            return new Compte(List.of(conseiller, client));
-        }
     }
-    /* Définition des variables de test */
-    private Client client = new CompteConfig().client();
-    private ConseillerFinancier conseiller = new CompteConfig().conseiller();
-    private Compte compte = new CompteConfig().compte(conseiller, client);
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+    Compte compte = (Compte) context.getBean("compte");
 
     /* Test de l'ajout d'un solde négatif
        On vérifie si la mise à jour du solde fonctionne, et on peut ensuite vérifier si le message
@@ -57,18 +48,18 @@ public class CompteTest {
         compte.effectuerTransaction(0);
         assertThat(compte.getSolde()).isEqualTo(0);
     }
-    /* Test vérifiant si le conseiller du compte mis à jour reçoit bien la notification appropriée */
+    /* Test vérifiant si le premier observateur du compte mis à jour reçoit bien la notification appropriée */
     @Test
     void testConseillerRecuNotification() {
         compte.effectuerTransaction(10);
-        assertThat(conseiller.getReceivedNotification()).isEqualTo("Le compte est maintenant créditeur");
+        assertThat(compte.getObservateurs().get(0).getReceivedNotification()).isEqualTo("Le compte est maintenant créditeur");
     }
 
-    /* Test vérifiant si le client du compte mis à jour reçoit bien la notification appropriée */
+    /* Test vérifiant si le deuxième observateur du compte mis à jour reçoit bien la notification appropriée */
     @Test
     void testClientRecuNotification() {
         compte.effectuerTransaction(10);
-        assertThat(client.getReceivedNotification()).isEqualTo("Le compte est maintenant créditeur");
+        assertThat(compte.getObservateurs().get(1).getReceivedNotification()).isEqualTo("Le compte est maintenant créditeur");
     }
 
 
